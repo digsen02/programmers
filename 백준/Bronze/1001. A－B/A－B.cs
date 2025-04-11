47d13f46b660d02ac73d966f75d75da4e6a53c32
digsen02 <165138438+digsen02@users.noreply.github.com>
2025-04-11 14:59:00 +0900

    [Bronze V] Title: A-B, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace 백준
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            
+            Console.WriteLine(a - b);
+        }
+    }
+}
