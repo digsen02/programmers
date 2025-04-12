3df6a99da9df3d79d7ef9c6ae2b67d8e2ea344f4
digsen02 <165138438+digsen02@users.noreply.github.com>
2025-04-12 23:14:28 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,18 @@
+namespace 백준
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()!.Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+        
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
