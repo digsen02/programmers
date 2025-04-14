ff2ab5ad23db92816368e4928add23e9b73cd507
digsen02 <165138438+digsen02@users.noreply.github.com>
2025-04-14 22:00:13 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace 백준 
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+         {
+             string? input = Console.ReadLine();
+             Console.WriteLine($"{input}??!");
+         }
+    }
+}
