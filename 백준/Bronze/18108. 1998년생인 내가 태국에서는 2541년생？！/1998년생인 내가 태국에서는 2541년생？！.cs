b4d701cdd880326bc3b0558e1ee4220c1662c7fc
digsen02 <165138438+digsen02@users.noreply.github.com>
2025-04-14 22:07:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,11 @@
+namespace 백준
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine()!);
+            Console.WriteLine(input - 543);
+        }
+    }
+}
